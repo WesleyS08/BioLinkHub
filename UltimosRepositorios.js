21c3e757a2a0c38c84c7461d76b6393ba3c11410
Wesley Silva <ws78009@gmail.com>
2024-05-29 15:27:14 -0300

    :bug: fix: correções de segurança

--- UltimosRepositorios.js
+++ UltimosRepositorios.js
@@ -1,27 +1,38 @@
+import config from './config.js'; // Importando o arquivo de configuração
 const username = 'WesleyS08';
+const GITHUB_TOKEN = config.GITHUB_TOKEN;
 
-async function fetchGitHubData() {
-    const reposResponse = await fetch('https://raw.githubusercontent.com/seu-usuario/seu-repositorio/main/repos.json');
-    const userResponse = await fetch('https://raw.githubusercontent.com/seu-usuario/seu-repositorio/main/user.json');
-    const repos = await reposResponse.json();
-    const userData = await userResponse.json();
-    return { repos, userData };
+
+
+async function fetchRepos() {
+    const response = await fetch(`https://api.github.com/users/${username}/repos?sort=created&direction=desc`, {
+        headers: {
+            'Authorization': `token ${GITHUB_TOKEN}`
+        }
+    });
+    const repos = await response.json();
+    return repos;
+}
+
+async function fetchUserStats() {
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+        headers: {
+            'Authorization': `token ${GITHUB_TOKEN}`
+        }
+    });
+    const userData = await response.json();
+    return userData;
 }
 
 async function displayRepos() {
-    const data = await fetchGitHubData();
-    const repos = data.repos;
+    const repos = await fetchRepos();
     const reposContainer = document.getElementById('github-repos');
-
-    // Limpar qualquer conteúdo pré-existente dentro do container
     reposContainer.innerHTML = '';
-
-    // Pegar apenas os 4 últimos repositórios
     const latestRepos = repos.slice(0, 4);
 
     latestRepos.forEach(repo => {
         const repoElement = document.createElement('div');
-        repoElement.className = 'repo-card'; // Adicionando a classe repo-card
+        repoElement.className = 'repo-card';
         repoElement.innerHTML = `
             <a href="${repo.html_url}" target="_blank" class="repo-link">
                 <h3>${repo.name}</h3>
@@ -33,11 +44,14 @@ async function displayRepos() {
 }
 
 async function displayUserStats() {
-    const data = await fetchGitHubData();
-    const userData = data.userData;
+    const userData = await fetchUserStats();
     const statsSpan = document.getElementById('github-stats');
     statsSpan.textContent = `Repos: ${userData.public_repos}, Followers: ${userData.followers}, Following: ${userData.following}`;
 }
 
-displayRepos();
-displayUserStats();
+function init() {
+    displayRepos();
+    displayUserStats();
+}
+
+init(); // Chamar a função de inicialização para iniciar o processo
---
+++ config.js
@@ -0,0 +1,7 @@
+
+// config.js
+const config = {
+    GITHUB_TOKEN: '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'
+};
+
+export default config;
